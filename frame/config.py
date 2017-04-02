bbcb8e53f34e9de1d97565a4f3a3fc8cc18deefa
hanwang <hanwang1985@126.com>
2017-04-02 13:05:09 +0800

    数据库测试

--- frame/config.py
+++ frame/config.py
@@ -10,7 +10,8 @@ import os
 DEBUG = True
 
 #--  bounty db config --
-BOUNTY_DB_HOST = "192.168.122.95"
+#BOUNTY_DB_HOST = "192.168.122.95"
+BOUNTY_DB_HOST = "127.0.0.1"
 BOUNTY_DB_PORT = 3306
 BOUNTY_DB_USER = "root"
 BOUNTY_DB_PASSWD = "root"
