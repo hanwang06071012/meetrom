0129c18c942c030bcbddbd1f8d6192445e343a7d
hanwang <hanwang1985@126.com>
2017-04-02 13:42:30 +0800

    加入secret_key

--- pro/__init__.py
+++ pro/__init__.py
@@ -4,5 +4,6 @@ from flask import Flask
 #-----------create app----------
 app = Flask(__name__)
 app.config.from_object("frame.config")
+app.config['SECRET_KEY'] = '123456'
 
 from view import default
