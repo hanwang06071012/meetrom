0487b4314765bf2c66baf9b737982cd712bce48a
hanwang <hanwang1985@126.com>
2017-04-02 21:36:35 +0800

    中文测试

--- pro/view/default.py
+++ pro/view/default.py
@@ -22,7 +22,8 @@ def show_default():
 	print("=====================start ========================")
 	print (current_name)
 	print str(current_name[0])
-	session['default_name'] = current_name[0]
+    dafault_name = json.dumps(current_name[0])
+	session['default_name'] = default_name
 	print("=====================  end ========================")
 	return render_template('default.html',**locals())
 
