e95480fd215549c0dbe03275089767d98d3983da
hanwang <hanwang1985@126.com>
2017-04-02 13:34:49 +0800

    修改使用session

--- pro/view/default.py
+++ pro/view/default.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask,session
 from flask import render_template
 from pro import app
 from pro.model.node_info  import NodeInfo
 
 @app.route('/')
 def show_default():
+	session['node_name'] = "beijing"
 	print("=====================start ========================")
 	str_sel = NodeInfo.select(cols="name")
 	print str_sel
 	print("=====================  end ========================")
-	return render_template('default.html')
+	return render_template('default.html',**local())
 
 @app.route('/fangwuchuzu')
 def fangwuchuzu():
