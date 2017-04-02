9bf5e99c3d0ca5f9e168c425141a024ac92dc516
hanwang <hanwang1985@126.com>
2017-04-02 20:13:05 +0800

    打印中文测试

--- pro/view/default.py
+++ pro/view/default.py
@@ -19,7 +19,7 @@ def show_default():
 		current_id = request.form['current_id',1]
 	current_name = NodeInfo.select(cols='name',where=("id = %s"% current_id))[0]
 	print("=====================start ========================")
-	print (current_name)
+	print unicode(current_name,'gbk')
 	print str(json.dumps(current_name))
 	session['default_name'] = json.dumps(current_name)
 	print("=====================  end ========================")
