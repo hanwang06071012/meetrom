66b5c25b2261f1a0ba2aac99e52b70f5124543d6
hanwang <hanwang1985@126.com>
2017-04-03 21:26:13 +0800

    加入备选城市

--- pro/view/default.py
+++ pro/view/default.py
@@ -16,12 +16,20 @@ sys.setdefaultencoding('utf-8')
 @app.route('/',methods=['POST','GET'])
 def show_default():
     current_id = 1
+    tuple_slave_name =()
     if request.method=='POST':
         current_id = request.form['current_id',1]
-    tuple_name = NodeInfo.select(cols='name',where=('node_id =%s'%(current_id)))
+    tuple_master_name = NodeInfo.select(cols='name',where=('node_id = %s'%(current_id)))
+    if (current_id > 0 and current_id <= 3133):
+        tuple_slave_name = NodeInfo.select(cols="name",where=('node_id > %s and node_id <= %s' % (current_id,current_id+3)))
+    else:
+        tuple_slave_name = NodeInfo.select(cols='name',where=('node_id = %s or node_id = %s or node_id = %s ' % (1,2,3)))
     print("=====================start ========================")
     print (tuple_name)
-    session['node_name'] = tuple_name[0][0]
+    session['master_city_name'] = tuple_master_name[0][0]
+    session['frist_slave_name'] = tuple_slave_name[0][0]
+    session['sed_slave_name'] = tuple_slave_name[1][0]
+    session['thrid_slave_name'] = tuple_slave_name[2][0]
     print("=====================  end ========================")
     return render_template('default.html',**locals())
 
