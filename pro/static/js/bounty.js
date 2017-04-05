e131581cb8316ae2544a6e6fe8abf5e07503bbab
hanwang <hanwang1985@126.com>
2017-04-05 23:38:20 +0800

    加入返回上一页地址设置函数

--- pro/static/js/bounty.js
+++ pro/static/js/bounty.js
@@ -1,8 +1,10 @@
+//测试用例
 function test()
 {
 	alert("I am in testing ......");
 }
 
+//设置城市
 function set_addr(addr_node_id)
 {
      $.post("/set/addr",
@@ -14,6 +16,26 @@ function set_addr(addr_node_id)
         if (msg=="")
         {
             location.reload();
+        }
+        else
+        {
+            alert(msg);
+        }
+    });
+}
+
+//设置城市并把回到上一页
+function set_addr_go_back(addr_node_id)
+{
+     $.post("/set/addr",
+    {
+        str_node_id:addr_node_id
+    },
+    function(msg,status)
+    {
+        if (msg=="")
+        {
+            history.back(-1);
         }
         else
         {
