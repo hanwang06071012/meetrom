8dc7fd3d573aab1d64ba319490d11e36ecb8b8e0
hanwang <hanwang1985@126.com>
2017-04-09 12:39:40 +0800

    动态增加房租水平

--- pro/static/js/bounty.js
+++ pro/static/js/bounty.js
@@ -64,14 +64,38 @@ function set_fangwu_chaxun()
             var list_city_name_id = json_msg[0];
             var str_city_labels = ("<label for='child_city'> <input type='radio' value='0' style='visibility: hidden;' name='child_city' id='0'/> 不限 </label>");
             for (var i = 0; i <list_city_name_id.length; i++) {
-                    var map_city_name_id = list_city_name_id[i];
-                    var city_val = map_city_name_id['id'];
-                    var city_id = ("city"+map_city_name_id['id']);
-                    var city_name = map_city_name_id['city_name'];
-                    var str_city_label = ("<label for='child_city'> <input type='radio' value=" + city_val +" style='visibility: hidden;' name='child_city' id='"+ city_id +"'/>"+ city_name +"</label>");
-                    str_city_labels += str_city_label;                
+                var map_city_name_id = list_city_name_id[i];
+                var city_val = map_city_name_id['id'];
+                var city_id = ("city"+map_city_name_id['id']);
+                var city_name = map_city_name_id['city_name'];
+                var str_city_label = ("<label for='child_city'> <input type='radio' value=" + city_val +" style='visibility: hidden;' name='child_city' id='"+ city_id +"'/>"+ city_name +"</label>");
+                str_city_labels += str_city_label;                
             }
             $("#child_city_names").html(str_city_labels);
+
+            var list_fangzu = json_msg[1];
+            var str_fangzu_labels = ("<p><label for='fang_zus'> 租金:</label>");
+            for (var i = 0; i < list_fangzu.length; i++) {
+                var fangzu_val = list_fangzu[i]
+                var fangzu_name ='';
+                if (i == 0)
+                {
+                    fangzu_name = (list_fangzu[i] + "元以下");
+                }
+                else if (i == (list_fangzu.length-1)) 
+                {
+                    fangzu_name = (list_fangzu[i] + "元以上");
+                }
+                else
+                {
+                    fangzu_name = (list_fangzu[i] + "-"+list_fangzu[i+1] + "元");
+                }
+                var fangzu_id = ("fangzu"+ str(i))
+                var str_fangzu_label = ("<label for='fang_zus'> <input type='radio' value=" + fangzu_val +" style='visibility: hidden;' name='fang_zu' id='"+ fangzu_id +"'/>"+ fangzu_name +"</label>");
+                str_fangzu_labels += str_fangzu_label;
+            }
+            str_fangzu_labels += "</p>";
+            $("#city_fangzu_levels").html(str_fangzu_labels);
         }
     });
 }
