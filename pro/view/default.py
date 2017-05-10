696ea62a833b3e0ad952cbcf3a952ea12cfaeb2d
hanwang <hanwang1985@126.com>
2017-05-10 22:37:39 +0800

    优化处理函数

--- pro/view/default.py
+++ pro/view/default.py
@@ -9,140 +9,14 @@
 from flask import Flask,session,url_for
 from flask import render_template,request
 from pro import app
-from pro.model.node_info  import NodeInfo
-from pro.model.income import Income
-from pro.model.estate_agents import EstateAgents
+from pro.model.admin  import Admin
+from pro.model.conference import Conference
+from pro.model.storepicture import StorePicture
+from pro.model.users import Users
 import json,sys
 from frame import const
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-def init_addr(current_id):
-    #current_id = const.DEFAULTCITYID
-    tuple_slave_name =()
-    #if request.method=='GET':
-        #current_id = request.values.get('current_id',default=const.DEFAULTCITYID)
-    current_id = int(current_id)
-    if (current_id > const.INITCITYNODEID and current_id <= (const.MAXCITYNODEID - const.CITYNODEIDOFFSET)):
-        tuple_slave_name = NodeInfo.select(cols="name",where=('node_id > %s and node_id <= %s' % (current_id,current_id+const.CITYNODEIDOFFSET)))
-        session['master_city_id'] = current_id
-        session['frist_slave_city_id'] = current_id + const.CITYNODEIDOFFSETONE
-        session['sed_slave_city_id'] = current_id + const.CITYNODEIDOFFSESED
-        session['thrid_slave_city_id'] = current_id + const.CITYNODEIDOFFSET
-    elif (current_id < const.INITCITYNODEID or current_id > const.MAXCITYNODEID):
-        current_id = const.DEFAULTCITYID
-        session['master_city_id'] = current_id
-        session['frist_slave_city_id'] = const.CITYNODEIDSED
-        session['sed_slave_city_id'] = const.CITYNODEIDTHREE
-        session['thrid_slave_city_id'] = const.CITYNODEIDFOURTH
-        tuple_slave_name = NodeInfo.select(cols='name',where=('node_id = %s or node_id = %s or node_id = %s ' % (const.CITYNODEIDSED,const.CITYNODEIDTHREE,const.CITYNODEIDFOURTH)))
-    else:
-        session['master_city_id'] = current_id
-        session['frist_slave_city_id'] = const.CITYNODEIDONE
-        session['sed_slave_city_id'] = const.CITYNODEIDSED
-        session['thrid_slave_city_id'] = const.CITYNODEIDTHREE
-        tuple_slave_name = NodeInfo.select(cols='name',where=('node_id = %s or node_id = %s or node_id = %s ' % (const.CITYNODEIDONE,const.CITYNODEIDSED,const.CITYNODEIDTHREE)))
-    tuple_master_name = NodeInfo.select(cols='name',where=('node_id = %s'%(session['master_city_id'])))
-    session['master_city_name'] = tuple_master_name[0][const.CITYLOCATIONINARRAY]
-    session['frist_slave_city_name'] = tuple_slave_name[0][const.CITYLOCATIONINARRAY]
-    session['sed_slave_city_name'] = tuple_slave_name[1][const.CITYLOCATIONINARRAY]
-    session['thrid_slave_city_name'] = tuple_slave_name[2][const.CITYLOCATIONINARRAY]
-
-#检查地址是否存在，不存在就初始化
-def check_addr():
-    try:
-        current_id = session['master_city_id']
-        if (len(session['master_city_name'])==0):
-            if (current_id==''):
-                init_addr(const.DEFAULTCITYID)
-            else:
-                init_addr(current_id)
-        else:
-            if (current_id !=''):
-                init_addr(current_id)
-            else:
-                pass
-    except:
-        init_addr(const.DEFAULTCITYID)
-    return ""
-
-#地址设置
-@app.route("/set/addr",methods=['POST'])
-def set_addr():
-    try:
-        current_city_id = request.values.get("str_node_id",default="")
-        if (len(current_city_id) != 0):
-            init_addr(current_city_id)
-            return ""
-        else:
-            return "set city failure...."    
-    except:
-        return "set city failure...."
-
-@app.route('/',methods=['POST','GET'])
-def show_default():
-    check_addr()
-    return render_template('default.html',**locals())
-
-@app.route('/fangwuchuzu',methods=['POST','GET'])
-def fangwuchuzu():
-    check_addr()
-    return render_template('sub1/fangwuchuzu.html',**locals())
-
-@app.route('/fangwuchaxun/init',methods=['POST','GET'])
-def init_fangwu_chaxun():
-    check_addr()
-    list_city_names = []
-    list_ciyt_fangzu=[]
-    list_agent_names=[]
-    map_fangchan_chaxun = {}
-    list_fangwu_chaxun_result=[]
-
-    current_city_id = session['master_city_id']
-    tuple_city_names=NodeInfo.select(cols='name,node_id',where=("parent_id=%s" % current_city_id))
-    try:
-        for sig_city_name in tuple_city_names:
-            map_city_name_id ={}
-            map_city_name_id['city_name'] = sig_city_name[0]
-            map_city_name_id['node_id'] = sig_city_name[1]
-            list_city_names.append(map_city_name_id)
-    except:
-        pass
-    #map_fangchan_chaxun["city_names"] = list_city_names
-    list_fangwu_chaxun_result.append(list_city_names)
-    #map_fangchan_chaxun={}
-
-    tuple_city_category=NodeInfo.select(cols='category',where=("node_id=%s" % current_city_id))
-    current_category = tuple_city_category[0][0]
-    tuple_city_incomes = Income.select(cols="frist_level,second_level,thrid_level,forth_level,fifth_level,sixth_level,seventh_level,eigth_level",where=("city_type=%s" % current_category))
-    try:
-        for sig_city_incomes in tuple_city_incomes:
-            for sig_city_income in sig_city_incomes:
-                sig_city_income = int(sig_city_income)
-                list_ciyt_fangzu.append(sig_city_income/5)
-    except:
-        pass
-    #map_fangchan_chaxun["fang_zu"] = list_ciyt_fangzu
-    list_fangwu_chaxun_result.append(list_ciyt_fangzu)
-
-    tuple_agent_names = EstateAgents.select(cols="name,id",where=("level >= %s" % current_category))
-    try:
-        for sig_agent_name in tuple_agent_names:
-            map_agent_name_id = {}
-            map_agent_name_id['name'] = sig_agent_name[0]
-            map_agent_name_id['id'] = sig_agent_name[1]
-            list_agent_names.append(map_agent_name_id)
-    except:
-        pass
-    #map_fangchan_chaxun['agents_names'] = list_agent_names
-    list_fangwu_chaxun_result.append(list_agent_names)
-
-    json_fangchan_chaxun_result = json.dumps(list_fangwu_chaxun_result)
-    return (json_fangchan_chaxun_result)
-
-@app.route('/selectaddr')
-def selectaddr():
-	return render_template('selectaddr.html')
 
 @app.route('/test')
 def test():
