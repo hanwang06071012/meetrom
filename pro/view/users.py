582ea9fc79c8910aa809b60c7d51326d52e8eb15
hanwang <hanwang1985@126.com>
2017-05-13 13:51:50 +0800

    增加用户查询展示

--- pro/view/users.py
+++ pro/view/users.py
@@ -28,7 +28,6 @@ def userlist():
 #添加展示用户
 @app.route("/users/add",methods=["POST","GET"])
 def user_add():
-    print(request.method)
     if request.method == 'POST':
         str_id =request.form["id"]
         user_name =request.form["user_name"]
@@ -45,13 +44,76 @@ def user_add():
         address =request.form["address"]
         user_mail=request.form["user_mail"]
         contract_phone =request.form["contract_phone"]
-        print("id=%s,user_name=%s"%(id,user_name))
         map_data = {"usersid":str_id,"usersName":user_name,"usersPass":passwd,"usersRepass":confirm_password,"usersTrueName":real_name,"usersAge":age,"usersSex":sex,"usersCsrq":date_birth,"usersHuji":household_registration,"usersJiguan":native_place,"usersIDcard":ID_number,"usersSpecialty":professional_title,"usersAddress":address,"usersEmail":user_mail,"usersPhone":contract_phone}
-        print("usersid=%s,userName=%s"%(str_id,user_name))
-        print(map_data)
         Users.insert(map_data)
         return redirect(url_for("userlist"))
     else:
         pass
     return render_template("users_add.html")
+
+    #添加展示用户
+@app.route("/users/select",methods=["POST","GET"])
+def user_select():
+    map_where ={}
+    list_key_val = []
+    str_sql_where=""
+    if request.method == 'POST':
+        str_id =request.form["id"]
+        if len(str_id) != 0:
+            map_where["usersid"] = str_id
+        user_name =request.form["user_name"]
+        if len(user_name) != 0:
+            map_where["usersName"] = user_name
+        real_name =request.form["real_name"]
+        if len(real_name) != 0:
+            map_where["usersTrueName"] = real_name
+        age =request.form["age"]
+        if len(age) != 0:
+            map_where["usersAge"] = age
+        sex=request.form["sex"]
+        if len(sex) != 0:
+            map_where["usersSex"] = sex
+        household_registration =request.form["household_registration"]
+        if len(household_registration) != 0:
+            map_where["usersHuji"] = household_registration
+        native_place=request.form["native_place"]
+        if len(native_place) != 0:
+            map_where["usersJiguan"] = native_place
+        ID_number=request.form["ID_number"]
+        if len(ID_number) != 0:
+            map_where["usersIDcard"] = ID_number
+        professional_title=request.form["professional_title"]
+        if len(professional_title) != 0:
+            map_where["usersSpecialty"] = professional_title
+        address =request.form["address"]
+        if len(address) != 0:
+            map_where["usersAddress"] = address
+        user_mail=request.form["user_mail"]
+        if len(user_mail) != 0:
+            map_where["usersEmail"] = user_mail
+        contract_phone =request.form["contract_phone"]
+        if len(contract_phone) != 0:
+            map_where["usersPhone"] = contract_phone
+        if len(map_where) != 0:
+            for key,val in map_where:
+                tuple_mid_key_val = (key,val)
+                list_key_val.append(tuple_mid_key_val)
+        int_len_list_key_val = len(list_key_val)
+        if int_len_list_key_val == 1:
+            str_sql_mid = (" %s like '%%%s%%'" % (list_key_val[0][0],list_key_val[0][1]))
+            str_sql_where += str_sql_mid
+        elif int_len_list_key_val > 1:
+            for i in range(int_len_list_key_val-1):
+                str_sql_mid = (" %s like %%%s%% and " % (list_key_val[i][0],list_key_val[i][1]))
+                str_sql_where += str_sql_mid
+            str_sql_mid += (" %s like %%%s%%" % (list_key_val[int_len_list_key_val-1][0],list_key_val[int_len_list_key_val-1][1]))
+            str_sql_where += str_sql_mid
+        else:
+            pass
+        str_col = ("%s,%s,%s,%s,%s,%s" % ("ID","usersid","usersName","usersPhone","usersEmail","createDate"))
+        tuple_result = Users.select(cols=str_col,where=str_sql_where)
+        return render_template("users_list.html",**locals())
+    else:
+        pass
+    return render_template("users_select.html");
 
