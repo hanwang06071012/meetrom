18afb5f466941c1d8bbacc0cca35385e87e3c0b7
hanwang <hanwang1985@126.com>
2017-05-13 23:58:13 +0800

    优化输出

--- pro/view/users.py
+++ pro/view/users.py
@@ -107,6 +107,7 @@ def user_select():
         print(int_len_list_key_val)
         if int_len_list_key_val == 1:
             str_sql_mid = (" %s like '%%%s%%'" % (list_key_val[0][0],list_key_val[0][1]))
+            print (str_sql_mid)
             str_sql_where += str_sql_mid
             print(str_sql_where)
         elif int_len_list_key_val > 1:
