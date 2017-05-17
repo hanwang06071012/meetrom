a1813c7b28313ddc89412d1cc7ac0c3f194d9261
hanwang <hanwang1985@126.com>
2017-05-17 20:02:01 +0800

    增加登陆验证

--- pro/view/admin.py
+++ pro/view/admin.py
@@ -40,52 +40,55 @@ def admin_info(id):
         return redirect(url_for("userlist"))
 
 #管理员修改个人资料
-@app.route("/admin/<id>/edit",methods=["POST","GET"])
-def admin_info_edit(id):
+@app.route("/admin/<id>/<level>/edit",methods=["POST","GET"])
+def admin_info_edit(id,level):
     map_where={}
-    if request.method == "POST":
-        AdminName =request.form["adminName"]
-        if len(AdminName) != 0:
-            map_where["AdminName"] = AdminName
-        AdminTrueName =request.form["adminTrueName"]
-        if len(AdminTrueName) != 0:
-            map_where["AdminTrueName"] = AdminTrueName
-        AdminAddress =request.form["adminAddress"]
-        if len(AdminAddress) != 0:
-            map_where["AdminAddress"] = AdminAddress
-        AdminEmail =request.form["adminEmail"]
-        if len(AdminEmail) != 0:
-            map_where["AdminEmail"] = AdminEmail
-        AdminSpecialty =request.form["adminSpecialty"]
-        if len(AdminSpecialty) != 0:
-            map_where["AdminSpecialty"] = AdminSpecialty
-        AdminHuji =request.form["adminHuji"]
-        if len(AdminHuji) != 0:
-            map_where["AdminHuji"] = AdminHuji
-        AdminIDcard =request.form["adminIDcard"]
-        if len(AdminIDcard) != 0:
-            map_where["AdminIDcard"] = AdminIDcard
-        AdminCsrq =request.form["adminCsrq"]
-        if len(AdminCsrq) != 0:
-            map_where["AdminCsrq"] = AdminCsrq
-        AdminJiguan =request.form["adminJiguan"]
-        if len(AdminJiguan) != 0:
-            map_where["AdminJiguan"] = AdminJiguan
-        AdminAge =request.form["adminAge"]
-        if len(AdminAge) != 0:
-            map_where["AdminAge"] = AdminAge
-        AdminSex =request.form["adminSex"]
-        if len(AdminSex) != 0:
-            map_where["AdminSex"] = AdminSex
-        AdminPhone =request.form["adminPhone"]
-        if len(AdminPhone) != 0:
-            map_where["AdminPhone"] = AdminPhone
-        str_sql_where = (" ID = %s")
-        Admin.update_dict(map_where,str_sql_where,[id])
-        return redirect(url_for("admin_list"))
-    str_col = ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % ("Adminid","AdminName","AdminTrueName","AdminAddress","AdminEmail","AdminSpecialty","AdminHuji","AdminIDcard","AdminCsrq","AdminJiguan","AdminAge","AdminSex","AdminPhone"))
-    tuple_result = Admin.select(cols=str_col,where=(" ID = %s" % (id)))
-    return render_template("admin_edit.html",**locals())
+    if level == '0':
+        if request.method == "POST":
+            AdminName =request.form["adminName"]
+            if len(AdminName) != 0:
+                map_where["AdminName"] = AdminName
+            AdminTrueName =request.form["adminTrueName"]
+            if len(AdminTrueName) != 0:
+                map_where["AdminTrueName"] = AdminTrueName
+            AdminAddress =request.form["adminAddress"]
+            if len(AdminAddress) != 0:
+                map_where["AdminAddress"] = AdminAddress
+            AdminEmail =request.form["adminEmail"]
+            if len(AdminEmail) != 0:
+                map_where["AdminEmail"] = AdminEmail
+            AdminSpecialty =request.form["adminSpecialty"]
+            if len(AdminSpecialty) != 0:
+                map_where["AdminSpecialty"] = AdminSpecialty
+            AdminHuji =request.form["adminHuji"]
+            if len(AdminHuji) != 0:
+                map_where["AdminHuji"] = AdminHuji
+            AdminIDcard =request.form["adminIDcard"]
+            if len(AdminIDcard) != 0:
+                map_where["AdminIDcard"] = AdminIDcard
+            AdminCsrq =request.form["adminCsrq"]
+            if len(AdminCsrq) != 0:
+                map_where["AdminCsrq"] = AdminCsrq
+            AdminJiguan =request.form["adminJiguan"]
+            if len(AdminJiguan) != 0:
+                map_where["AdminJiguan"] = AdminJiguan
+            AdminAge =request.form["adminAge"]
+            if len(AdminAge) != 0:
+                map_where["AdminAge"] = AdminAge
+            AdminSex =request.form["adminSex"]
+            if len(AdminSex) != 0:
+                map_where["AdminSex"] = AdminSex
+            AdminPhone =request.form["adminPhone"]
+            if len(AdminPhone) != 0:
+                map_where["AdminPhone"] = AdminPhone
+            str_sql_where = (" ID = %s")
+            Admin.update_dict(map_where,str_sql_where,[id])
+            return redirect(url_for("admin_list"))
+        str_col = ("%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s" % ("Adminid","AdminName","AdminTrueName","AdminAddress","AdminEmail","AdminSpecialty","AdminHuji","AdminIDcard","AdminCsrq","AdminJiguan","AdminAge","AdminSex","AdminPhone"))
+        tuple_result = Admin.select(cols=str_col,where=(" ID = %s" % (id)))
+        return render_template("admin_edit.html",**locals())
+    else:
+        pass
 
 #管理员密码修改
 @app.route("/admin/<id>/pwd",methods=["POST","GET"])
